18619bfb1a8702fcace02393d92698cbfd08051e
nico233 <1601550204@qq.com>
2023-06-21 14:57:05 +0800

    添加其他项

---
+++ BridgeCheckSystem/Other.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <QString>
+#include <QDateTime>
+
+// 其他项 
+// others
+class Other {
+private:
+	QString bridgeEngineer;
+	QString cardFiller;			// 填卡人
+	QDateTime cardTime;			// 填卡时间
+};
+
