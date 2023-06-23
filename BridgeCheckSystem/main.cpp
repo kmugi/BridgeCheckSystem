41d02b756626cf41cbf76d2a7e41e712d096a7d6
nico233 <1601550204@qq.com>
2023-06-23 21:31:36 +0800

    添加qss资源加载和miniblink加载

--- BridgeCheckSystem/main.cpp
+++ BridgeCheckSystem/main.cpp
@@ -1,10 +1,33 @@
 #include "Login.h"
 #include <QtWidgets/QApplication>
 
-int main(int argc, char *argv[])
+#include <QFile>
+#include <QString>
+
+#include "wke.h"
+
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
-    Login w;
-    w.show();
-    return a.exec();
+	QApplication a(argc, argv);
+
+	// load qss
+	QString qss{};
+	QFile qssFile{ "F:/Demo/project/BridgeCheckSystem/BridgeCheckSystem/qss/Ubuntu.qss" };
+	qssFile.open(QIODeviceBase::ReadOnly);
+	if (qssFile.isOpen()) {
+		qss = QLatin1String(qssFile.readAll());
+		qApp->setStyleSheet(qss);
+		qssFile.close();
+		qDebug() << "Successfully loaded the QSS resource file";
+	} else {
+		qDebug() << "Can't open the qss resource file";
+	}
+
+	// load wke dll and initialize
+	wkeSetWkeDllPath(L"F:/Demo/project/BridgeCheckSystem/BridgeCheckSystem/miniblink_4975_x64.dll");
+	wkeInit();
+
+	Login w;
+	w.show();
+	return a.exec();
 }
