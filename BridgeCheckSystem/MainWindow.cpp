21c4e0b2a475c453f0f921aca609625aa851286e
nico233 <1601550204@qq.com>
2023-06-23 21:31:45 +0800

    添加主窗口

---
+++ BridgeCheckSystem/MainWindow.cpp
@@ -0,0 +1,37 @@
+#include "MainWindow.h"
+
+MainWindow::MainWindow(QWidget* parent)
+	: QWidget(parent)
+{
+	ui.setupUi(this);
+
+	// Set to delete this object when the window is closed
+	setAttribute(Qt::WA_DeleteOnClose);
+
+	// show the amap on miniblink webview
+	showAmap();
+
+	connect(ui.newProfileBtn, &QPushButton::clicked, this, &MainWindow::on_newProfileBtn_clicked);
+	connect(ui.showProfileBtn, &QPushButton::clicked, this, &MainWindow::on_showProfileBtn_clicked);
+}
+
+void MainWindow::showAmap() {
+	const QString mapPath = "F:/Demo/project/BridgeCheckSystem/BridgeCheckSystem/amap/amap.html";
+	webView = wkeCreateWebWindow(WKE_WINDOW_TYPE_CONTROL, (HWND)ui.mapWidget->winId(), 0, 0, 1100, 800);
+	wkeShowWindow(webView, TRUE);
+	QFile file(mapPath);
+	file.open(QIODevice::ReadOnly);
+	wkeLoadHTML(webView, file.read(file.bytesAvailable()));
+}
+
+void MainWindow::on_newProfileBtn_clicked() {
+	BridgeProfileWidget* bpWidget = new BridgeProfileWidget();
+	bpWidget->setWindowModality(Qt::ApplicationModal);	// 模态窗口，阻塞一切窗口
+	auto title = QString("新建资料卡 类型: %1").arg(ui.bridgeTypes->currentText());
+	bpWidget->setWindowTitle(title);
+	bpWidget->show();
+}
+
+void MainWindow::on_showProfileBtn_clicked() {
+	// TODO: 写完接口以后写这个
+}
---
+++ BridgeCheckSystem/MainWindow.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <QWidget>
+#include <QFile>
+
+#include "ui_MainWindow.h"
+#include "wke.h"
+#include "BridgeProfileWidget.h"
+
+class MainWindow : public QWidget
+{
+	Q_OBJECT
+
+signals:
+	// TODO: 编写传输桥梁数据的信号函数
+
+public:
+	MainWindow(QWidget* parent = nullptr);
+	~MainWindow() {}
+
+	void showAmap();
+
+	void on_newProfileBtn_clicked();
+	void on_showProfileBtn_clicked();
+
+private:
+	wkeWebView webView{}; // miniblink container
+
+private:
+	Ui::MainWindowClass ui;
+};
